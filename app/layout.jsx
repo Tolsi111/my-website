86cd6d0319b402e13f54c5a2ac302d00bc13ce0f
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2024-07-16 19:03:46 +0300

    added base shapes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import styles from "./page.module.scss";
+import AnimatedBackground from "@/components/animated-background/AnimatedBackground";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <AnimatedBackground/>
       <body className={`${inter.className} ${styles.main}`}>{children}</body>
     </html>
   );
---
+++ components/animated-background/AnimatedBackground.jsx
@@ -0,0 +1,15 @@
+import styles from './AnimatedBackground.module.scss'
+
+export default function AnimatedBackground() {
+    return (
+        <div className={styles.background}>
+            <div className={`${styles.bottom} ${styles['bottom-first']}`}/>
+            <div className={`${styles.bottom} ${styles['bottom-second']}`}/>
+            <div className={styles['bottom-inner']}></div>
+
+            <div className={`${styles.top} ${styles['top-first']}`}/>
+            <div className={`${styles.top} ${styles['top-second']}`}/>
+            <div className={styles['top-inner']}></div>
+        </div>
+    )
+}
