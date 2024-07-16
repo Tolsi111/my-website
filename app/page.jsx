0be18986cd8a6f25acc23417cd16047287eba39c
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2024-07-16 17:55:19 +0300

    base grid layout (#1)

--- app/page.jsx
+++ app/page.jsx
@@ -3,17 +3,27 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
   <main className={styles['main-container']}>
-    <div className={styles['row-flex']}>
-      <section className={styles['contact']}></section>
-      <div className={styles['col-flex']}>
-        <section className={styles['side']}></section>
-        <section className={styles['hobbies']}></section>
-      </div>
-    </div>
-    <div className={styles['row-flex']}>
-      <section className={styles['experience']}></section>
-      <section className={styles['education']}></section>
-    </div>
+    <article className={`${styles.title} ${styles.flex}`}>
+title
+    </article>
+    <article className={`${styles.experience} ${styles.flex}`}>
+      {/* nav link */}experience
+    </article>
+    <article className={`${styles.contacts} ${styles.flex}`}>
+contacts
+    </article>
+    <article className={`${styles.me} ${styles.flex}`}>
+me
+    </article>
+    <article className={`${styles.projects} ${styles.flex}`}>
+      {/* nav link */}projects
+    </article>
+    <article className={`${styles.hobbies} ${styles.flex}`}>
+      {/* nav link */}hobbies
+    </article>
+    <article className={`${styles.education} ${styles.flex}`}>
+      {/* button */}education
+    </article>
   </main>
   );
 }
