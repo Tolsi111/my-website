765861655f138857d48c03eaa92e609f057d37bc
AndreiBacila <dey.bacila09@yahoo.com>
2025-01-26 12:50:46 +0200

    more on structure

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,25 +1,49 @@
 import styles from './projects-page.module.scss'
+import Link from "next/link";
+import NavigationArrow from "@/components/navigation-arrow/NavigationArrow";
+import ExternalLinkButton from "@/components/external-link-button/ExternalLinkButton";
 
 export default function Projects() {
     return (
         <main className={styles['main-container']}>
             <section className={styles['wheel-slider']} >
                 {'slider'}
+                <h3>Angular</h3>
             </section>
             <section className={styles['wheel-controls']} >
-            {'slider controls'}
+            <h3>Slider controls</h3>
+            {/* TODO: add control arrows */}
             </section>
             <section className={styles['project-title']} >
-                {'title'}
+                {/* TODO: dynamic title */}
+                <h1>Title of the project</h1>
             </section>
             <section className={styles['project-links']} >
-                {'links'}
+                <h3>Project links</h3>
+                {/* TODO: dynamic links based on current project. The repo might be private */}
+                <ul>
+                    <li><ExternalLinkButton targetUrl="https://github.com/Tolsi111" linkText="Preview site"/></li>
+                    <li><ExternalLinkButton targetUrl="https://github.com/Tolsi111" linkText="View on Github"/></li>
+                </ul>
+
             </section>
             <section className={styles['back-home']} >
-                {'back-home'}
+                <h3>Back to home page</h3>
+                <Link href={"../"} className={styles.navarrow}><NavigationArrow/></Link>
             </section>
             <section className={styles['project-description']} >
-                {'description'}
+                {/* TODO: dynamic description */}
+                <article className={styles['project-usage']}>
+                    <p>Ongoing</p>
+                    <p>Here should go a brief description of the project from a user's POV.</p>
+                </article>
+                <article className={styles['project-features']}>
+                    <p>Here should go a more technical description of the features.</p>
+                </article>
+
+                <article className={styles['project-tech-stack']}>
+                    <p>Here should go a more technical description of the features.</p>
+                </article>
             </section>
         </main>
     )
