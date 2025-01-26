b387e4febc19f02f018252b1029cea11b2691856
AndreiBacila <dey.bacila09@yahoo.com>
2025-01-26 12:18:56 +0200

    base layout

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,26 @@
+import styles from './projects-page.module.scss'
+
 export default function Projects() {
     return (
-        <h1>Projects</h1>
+        <main className={styles['main-container']}>
+            <section className={styles['wheel-slider']} >
+                {'slider'}
+            </section>
+            <section className={styles['wheel-controls']} >
+            {'slider controls'}
+            </section>
+            <section className={styles['project-title']} >
+                {'title'}
+            </section>
+            <section className={styles['project-links']} >
+                {'links'}
+            </section>
+            <section className={styles['back-home']} >
+                {'back-home'}
+            </section>
+            <section className={styles['project-description']} >
+                {'description'}
+            </section>
+        </main>
     )
 }
