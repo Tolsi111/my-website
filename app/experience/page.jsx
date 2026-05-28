2ff42a7810acf95fc4445affa27294cbf853cb92
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2026-05-28 12:42:17 +0300

    general layout

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -1,5 +1,28 @@
+import styles from "./experience-page.module.scss";
+import Link from "next/link";
+import NavigationArrow from "@/components/navigation-arrow/NavigationArrow";
+
 export default function Experience() {
-    return (
-        <h1>Experience</h1>
-    )
+  return (
+    <main className={styles["main-container"]}>
+      <section className={styles["back-home"]}>
+        <h3>Back to home page</h3>
+        <Link href={"../"} className={styles.navarrow}>
+          <NavigationArrow />
+        </Link>
+      </section>
+      <section className={styles["slider"]}>
+        <h3>Slider</h3>
+      </section>
+      <section className={styles["job-title"]}>
+        <h3>Job Title</h3>
+      </section>
+      <section className={styles["highlights"]}>
+        <h3>Highlights</h3>
+      </section>
+      <section className={styles["tech-stack"]}>
+        <h3>Tech Stack</h3>
+      </section>
+    </main>
+  );
 }
